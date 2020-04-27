343b900e440e15384680e86226a94e3b779a5ae7
Hyunsik Boo <bhs9194@nate.com>
2020-04-28 00:06:42 +0900

    Update event.sol

--- event.sol
+++ event.sol
@@ -1,34 +1,14 @@
 pragma solidity ^0.4.22;
 
-contract owned {
-    address owner;
-    
-    constructor() {
-        owner = msg.sender;
-    }
-    
-    modifier onlyOwner {
-        require(msg.sender == owner, 
-            "Only the contract owner can call this function");
-        _;
-    }
-}
-
-contract mortal is owned {
-    function destroy() public onlyOwner {
-        selfdestruct(owner);
-    }
-}
-
 contract Faucet {
+    
     event Withdrawal(address indexed to, uint amount);
     event Deposit(address indexed from, uint amount);
     
     function withdraw(uint withdraw_amount) public {
-        require(withdraw_amount <= 0.1 ether);
-        require(this.balance >= withdraw_amount,
-            "Insufficient balance in faucet for withdrawal request");
+        require(withdraw_amount <= 1000);
         msg.sender.transfer(withdraw_amount);
+        
         emit Withdrawal(msg.sender, withdraw_amount);
     }
     
