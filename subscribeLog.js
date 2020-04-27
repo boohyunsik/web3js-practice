433e1a0601af694ac774f9a97a584789c3c8f009
Hyunsik Boo <bhs9194@nate.com>
2020-04-28 02:26:16 +0900

    Update subscribeLog.js

--- subscribeLog.js
+++ subscribeLog.js
@@ -3,11 +3,11 @@ var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws/v3/595b7af287a44ac4876e614e31f797ef'));
 
 web3.eth.subscribe('logs', {
-	address: '0xc907d1ef772ba0b170b2a773ceb908391be24bda',
+	address: '0xa93f97e8a30447b27682fee9b10d6dcbe3711da2',
 	topics : [null]
 }, function(error, result) {
-	if (!error) {
-		console.log(result);
+	if (error) {
+		console.log("error", error);
 	}
 }).on("connected", function(subscriptionId) {
 	console.log("subscrpitionId : ", subscriptionId);
